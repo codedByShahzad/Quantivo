9d2f4983afe7c13c11c292ef659561c71b0c5320
MrShahzadDeveloper <mr.shahzad.developer@gmail.com>
2026-05-28 16:20:19 +0500

    Setup the Folders

---
+++ frontend/app/(landing)/layout.tsx
@@ -0,0 +1,20 @@
+import Navbar from "@/src/components/HomeNavbar";
+import Footer from "@/src/components/HomeFooter";
+
+export default function LandingLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+
+      <main className="flex-1">
+        {children}
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>Dashboard Over View</div>
+    <div>Dashboard Overview</div>
   )
 }
 
